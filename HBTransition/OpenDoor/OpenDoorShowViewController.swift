cc9475f7fadd8cd221308cccdca7ed2b02ea5180
jianghongbao <18218462815@163.com>
2017-11-18 18:00:02 +0800

    添加手势更新

--- HBTransition/OpenDoor/OpenDoorShowViewController.swift
+++ HBTransition/OpenDoor/OpenDoorShowViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class OpenDoorShowViewController: PulicViewController ,UINavigationControllerDelegate {
 
+    fileprivate var pan : UIPanGestureRecognizer?
     override func viewDidLoad() {
         super.viewDidLoad()
         icon = #imageLiteral(resourceName: "holk")
+        pan = UIPanGestureRecognizer.init(target: self, action: #selector(move(pan:)))
+        if let pan = self.pan {
+            view.addGestureRecognizer(pan)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,5 +28,27 @@ class OpenDoorShowViewController: PulicViewController ,UINavigationControllerDel
     func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationControllerOperation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         return OpenDoorManager.init(pathType: operation == UINavigationControllerOperation.push ? .push : .pop )
     }
-
+//UIPercentDrivenInteractiveTransition
+    @objc func move(pan:UIPanGestureRecognizer) -> () {
+        var percent : CGFloat = 0
+        let x = pan.translation(in: view).x
+        percent = x / view.frame.width
+        switch pan.state {
+        case .began:
+            self.navigationController?.popViewController(animated: true)
+            break
+        case .changed:
+            /*
+            CGFloat transitionX = [panGesture translationInView:panGesture.view].x;
+            persent = transitionX / panGesture.view.frame.size.width;
+             */
+            
+            break
+        case .ended:
+            
+            break
+        default:break
+        }
+    }
 }
+
