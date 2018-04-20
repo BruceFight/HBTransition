03ac4faa7a37dd57a4e3b24063027fc1a9ed2735
jianghongbao <18218462815@163.com>
2018-04-20 17:46:24 +0800

    Add process

--- HBTransition/Spring/SpringShowViewController.swift
+++ HBTransition/Spring/SpringShowViewController.swift
@@ -12,11 +12,12 @@ class SpringShowViewController: UIViewController ,UIViewControllerTransitioningD
 
     fileprivate var iconView = UIImageView()
     open var dismissHandler : (() -> ())?
-    
+    fileprivate var manger = DisInteractionManager()
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         self.transitioningDelegate = self
         self.modalPresentationStyle = .custom
+        manger.observeGestureFor(vc: self)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -55,10 +56,63 @@ extension SpringShowViewController {
     }
     /*
     func interactionControllerForPresentation(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
-        
+     
+    }
+    */
+    func interactionControllerForDismissal(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        return manger.interation ? manger : nil
+    }
+    /*
+     
+     - (id<UIViewControllerInteractiveTransitioning>)interactionControllerForDismissal:(id<UIViewControllerAnimatedTransitioning>)animator{
+     return _interactiveDismiss.interation ? _interactiveDismiss : nil;
+     }
+     
+     - (id<UIViewControllerInteractiveTransitioning>)interactionControllerForPresentation:(id<UIViewControllerAnimatedTransitioning>)animator{
+     XWInteractiveTransition *interactivePresent = [_delegate interactiveTransitionForPresent];
+     return interactivePresent.interation ? interactivePresent : nil;
+     }*/
+}
+
+class DisInteractionManager: UIPercentDrivenInteractiveTransition {
+    
+    var interation : Bool = false
+    fileprivate var pan : UIPanGestureRecognizer?
+    weak fileprivate var vc : UIViewController?
+    func observeGestureFor(vc:UIViewController) -> () {
+        self.vc = vc
+        self.pan = UIPanGestureRecognizer.init(target: self, action: #selector(move(pan:)))
+        if let pan = self.pan {
+            vc.view.addGestureRecognizer(pan)
+        }
     }
     
-    func interactionControllerForDismissal(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
-        
-    }*/
+    @objc func move(pan:UIPanGestureRecognizer) -> () {
+        var percent : CGFloat = 0
+        var y : CGFloat = 0
+        if let vc = self.vc {
+            y = pan.translation(in: vc.view).y
+            percent = y / 300//(vc.view.frame.height)
+        }
+        switch pan.state {
+        case .began:/// 同理dismiss,见OpenDoor
+            interation = true
+            //self.vc?.navigationController?.popViewController(animated: true)
+            self.vc?.dismiss(animated: true, completion: nil)
+            break
+        case .changed:
+            print("❤️ percent: \(percent)")
+            self.update(percent)
+            break
+        case .ended:
+            interation = false
+            if percent > 0.5 {
+                self.finish()
+            }else {
+                self.cancel()
+            }
+            break
+        default:break
+        }
+    }
 }
